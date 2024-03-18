433ff405f61eac995dccf70902b433500252fb5b
Atomnaphat <napat1970@gmail.com>
2024-03-18 15:54:44 +0700

    แก้ไข code

--- opencv.py
+++ opencv.py
@@ -1,12 +1,6 @@
 import cv2
 
-# เลือกวิธีการเชื่อมต่อกับกล้อง
-# ถ้าใช้กล้องในเครือข่าย (IP Camera) ใช้ rtsp หรือ http
-# ถ้าใช้กล้องในเครื่อง (Webcam) ใช้ตัวเลข 0
-# หากมีกล้องหลายตัวสามารถใส่ตัวเลขตามลำดับ เช่น 1, 2, 3, ...
-camera_source = "rtsp://your_ip_address/your_stream_path"  # หรือ "http://your_ip_address/video"
-# ถ้าใช้กล้องในเครื่อง (Webcam)
-# camera_source = 0
+camera_source = 1
 
 # เชื่อมต่อกับกล้อง
 cap = cv2.VideoCapture(camera_source)
